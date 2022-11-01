5c7a3d57b6dcf4c7c2aaca32acb63fefb800a601
DanielHatoomian <97552448+DanielHatoomian@users.noreply.github.com>
2022-11-01 09:44:49 -0700

    Update TTT.java

--- TTT.java
+++ TTT.java
@@ -2,11 +2,22 @@ import pkg.*;
 import java.util.ArrayList;
 
 public class TTT implements GameState {
+	int [] [] moves = new int[3][3];
 	boolean isGameOver() {
-
+	
 	}
 	Player getWinner() {
-
+		if(!isGameOver) {
+			if(winner==Player1) {
+				return Player1;
+			}
+			else if(winner==Player2) {
+				return Player2;
+			}
+			else {
+				return null;
+			}
+		}
 	}
 	Player getCurrentPlayer() {
 
