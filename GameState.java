bbc4df919211938dca280bd26097165b4193e185
derekg55 <75149469+derekg55@users.noreply.github.com>
2022-11-01 09:33:50 -0700

    Add files via upload

---
+++ GameState.java
@@ -0,0 +1,8 @@
+public interface GameState {
+	boolean isGameOver();
+	Player getWinner();
+	Player getCurrentPlayer();
+	ArrayList<String> getCurrentMoves();
+	void makeMove(String move);
+	String toString();
+}
---
+++ TTT.java
@@ -0,0 +1,23 @@
+import pkg.*;
+import java.util.ArrayList;
+
+public class TTT implements GameState {
+	boolean isGameOver() {
+
+	}
+	Player getWinner() {
+
+	}
+	Player getCurrentPlayer() {
+
+	}
+	ArrayList<String> getCurrentMoves() {
+
+	}
+	void makeMove(String move) {
+
+	}
+	String toString() {
+
+	}
+}
