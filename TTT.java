4a9808f03dbd7915ced21a29eef342233a02b9eb
DanielHatoomian <97552448+DanielHatoomian@users.noreply.github.com>
2022-11-01 10:01:39 -0700

    Update TTT.java

--- TTT.java
+++ TTT.java
@@ -4,7 +4,41 @@ import java.util.ArrayList;
 public class TTT implements GameState {
 	public static int[][] board = new int[3][3];
 	public boolean isGameOver() {
-
+		for(int i = 0; i<board.length; i++) {
+			if(board[i][0] == 1 && board[i][1] == 1 && board[i][2] == 1) {
+				winner = Player1;
+				return true;
+			}
+			else if(board[i][0] == 2 && board[i][1] == 2 && board[i][2] == 2) {
+				winner = Player2;
+				return true;
+			}
+			else if(board[0][i] == 1 && board[1][i] == 1 && board[2][i] == 1) {
+				winner = Player1;
+				return true;
+			}
+			else if(board[0][i] == 2 && board[1][i] == 2 && board[2][i] == 2) {
+				winner = Player2;
+				return true;
+			}
+		}
+		if(board[0][0] == 1 && board[1][1] == 1 && board[2][2] == 1) {
+			winner = Player1;
+			return true;
+		}
+		else if(board[0][0] == 2 && board[1][1] == 2 && board[2][2] == 2) {
+			winner = Player2;
+			return true;
+		}
+		else if(board[2][0] == 1 && board[1][1] == 1 && board[0][2] == 1) {
+			winner = Player1;
+			return true;
+		}
+		else if(board[2][0] == 2 && board[1][1] == 2 && board[0][2] == 2) {
+			winner = Player2;
+			return true;
+		}
+		return false;
 	}
 	public Player getWinner() {
 
