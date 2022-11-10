6eb44c9fa7c207e1b50f509e241474de1cb62955
RainS32 <117197592+RainS32@users.noreply.github.com>
2022-11-10 10:22:46 -0800

    Update Player.java

--- Player.java
+++ Player.java
@@ -1,15 +1,18 @@
-
+import pkg.*;
+import java.util.ArrayList;
 
 public class Player{
 	private String name;
 
-	public Player(String aName)
-	{name=aName;}
+	public Player(String aName) {
+		name=aName;
+	}
 
-	public String getName()
-	{return name;}
+	public String getName() {
+		return name;
+	}
 
 	public String getNextMove(GameState state) {
-		
+		return "hello";
 	}
 }
